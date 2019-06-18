6c851afb57a12e22f8c2fed3825fafb022033195
Robby Horsley <robert.horsley@tcstire.com>
2019-06-18 00:42:11 -0600

    finishing up CRUD ability for post - added authentication for users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,34 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale')}}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
-    <title>{{config('app.name', 'LSAPP')}}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    @include('inc.navbar')
-    <div class="container mt-5">
-        @include('inc.messages')
-        @yield('content')
+    <div id="app">
+        @include('inc.navbar')
+        <div class="container">
+            @include('inc.messages')
+        <main class="py-4">
+            @yield('content')
+        </main>
     </div>
+    
     <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
